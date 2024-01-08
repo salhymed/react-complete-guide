e4b2825ad57ec6080fd6b2315d68157c69eada39
salhysamed <salhy.mohamed@gmail.com>
2024-01-08 22:03:18 +0100

    adding cart modal state to hide/show the car

--- food-order-app/src/components/UI/Modal.jsx
+++ food-order-app/src/components/UI/Modal.jsx
@@ -4,17 +4,17 @@ import  ReactDOM  from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
-  return <div className={classes.modal}>{props.children}</div>;
+  return <div className={classes.modal} >{props.children}</div>;
 };
 const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </>
   );
