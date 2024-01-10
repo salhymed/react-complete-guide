5cb4ac2f1f0cce5aae4ff7e716d1c85a7bd7b6be
salhysamed <salhy.mohamed@gmail.com>
2024-01-10 13:53:33 +0100

    bunmping cart when an Item is added

--- food-order-app/src/components/Layout/HeaderCartButton.jsx
+++ food-order-app/src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
-  const ctx = useContext(CartContext)
-  const numberOfItems = ctx.items.reduce((curNumber, item)=>{
-    return curNumber + item.amount
-  },0)
+  const [btnIsPump, setBtnIsBump] = useState(false);
+  const ctx = useContext(CartContext);
+  const numberOfItems = ctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  const btnClasses = `${classes.button} ${btnIsPump ? classes.bump : ''}`;
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnIsBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBump(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
