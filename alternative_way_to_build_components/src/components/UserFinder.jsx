49af002a41b212f7f36c1368b7b175d8d1c4c9b2
salhysamed <salhy.mohamed@gmail.com>
2024-01-11 14:27:44 +0100

    class-bases comp. and life cyle methods

---
+++ alternative_way_to_build_components/src/components/UserFinder.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment, Component } from 'react';
+
+import Users from './Users';
+import classes from './UserFinder.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+class UserFinder extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: '',
+    };
+  }
+  searchChangeHandler(ev) {
+    this.setState({ searchTerm: ev.target.value });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+export default UserFinder;
