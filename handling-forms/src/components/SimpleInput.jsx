89c4b75f69157e2a45e00b1b027e100c1fd24654
salhysamed <salhy.mohamed@gmail.com>
2024-01-15 09:47:03 +0100

    basic validation

---
+++ handling-forms/src/components/SimpleInput.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+const SimpleInput = (props) => {
+  const [entredName, setEnetredName] = useState('');
+  const [nameInputTouched, setNameInputTouched] = useState(false);
+  const enteredNameIsValid = entredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && nameInputTouched;
+
+  const nameInputHandler = (ev) => {
+    setEnetredName(ev.target.value);
+  };
+
+  const formSubmitHandler = (ev) => {
+    ev.preventDefault();
+    setNameInputTouched(true);
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    setEnetredName('');
+    setNameInputTouched(false);
+  };
+
+  const nameBlurHandler = (ev) => {
+    setNameInputTouched(true);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div
+        className={nameInputIsInvalid ? 'form-control invalid' : 'form-control'}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={entredName}
+          onChange={nameInputHandler}
+          onBlur={nameBlurHandler}
+          type="text"
+          id="name"
+        />
+        {nameInputIsInvalid && <p className="error-text">name is invalid</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
